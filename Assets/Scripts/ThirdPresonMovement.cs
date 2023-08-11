246699e303e44cd1f482d3a941bec6cb1b721bba
Nishant-modi <mnishant0123@gmail.com>
2023-08-11 15:13:25 +0530

    Gravity and ground check

--- Assets/Scripts/ThirdPresonMovement.cs
+++ Assets/Scripts/ThirdPresonMovement.cs
@@ -11,9 +11,24 @@ public class ThirdPresonMovement : MonoBehaviour
     [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public Transform ceilingCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y<0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f , vertical).normalized;
@@ -27,5 +42,8 @@ public class ThirdPresonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle,0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
